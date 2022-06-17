c5660c014defd8f3106a1cc909faddb6bdcf72fa
Jayesh-Patidar <j.patidar1910@gmail.com>
2022-06-17 23:23:13 +0530

    Added navbar component with design

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,5 +1,37 @@
 import React from "react";
+import styles from "../../styles/Home.module.css";
+import navbarStyles from "../../styles/Navbar.module.css";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className={navbarStyles.navbar}>
+      <div
+        className={styles.container}
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <a href="#" className={styles.logo}>
+          Logo
+        </a>
+        <div className={navbarStyles.navItems}>
+          <a href="#" className={navbarStyles.navLinks}>
+            Skills
+          </a>
+          <a href="#" className={navbarStyles.navLinks}>
+            Projects
+          </a>
+          <a href="#" className={navbarStyles.navLinks}>
+            Content
+          </a>
+          <a href="#" className={navbarStyles.navLinks}>
+            About us
+          </a>
+          <button className={navbarStyles.resumeButton}>Resume</button>
+        </div>
+      </div>
+    </nav>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 import { Footer, Navbar } from "../components";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Navbar />
       <h1>Home</h1>
       <Footer />
