41a5f3d6aed54c589632a9974adcbbeededf5072
vivekbhatt07 <vivekbhatt618@gmail.com>
2022-06-18 14:42:39 +0530

    add: images

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import { Footer, Navbar } from "../components";
+import { Hero } from "../sections/home";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Navbar />
-      <h1>Home</h1>
-      <Footer />
+      {/* <Navbar /> */}
+      {/* <h1>Home</h1> */}
+      {/* <Hero /> */}
+      {/* <Footer /> */}
+      <Hero />
     </div>
   );
 }
